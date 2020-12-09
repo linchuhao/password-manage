27f0c4dce6f61a2acc87ead9d7e9c224a003a5b0
Lester Lin <LINLE5@OOCL.COM>
2020-12-09 09:57:16 +0800

    [Lester]password manage demo.

---
+++ src/main/java/com/example/demo/exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException{
+    @Override
+    public String getMessage() {
+        return "EmployeeNotFoundException";
+    }
+}
